3ad9226436d4cc857e5686b6af78a61955995bc5
kumang-subba <kumangsubba@gmail.com>
2023-11-27 18:05:30 +0200

    pathname change

--- components/Navigation/DashboardNavbar.tsx
+++ components/Navigation/DashboardNavbar.tsx
@@ -14,7 +14,7 @@ const DashboardNavbar = () => {
         <button
           className="group flex items-center relative outline-none border-none focus:border-none focus:outline-none ring-0"
           onClick={() => {
-            if (pathname.split("/")[1] !== "dashboard") {
+            if (pathname?.split("/")[1] !== "dashboard") {
               router.push("/dashboard");
             }
             return;
@@ -23,15 +23,15 @@ const DashboardNavbar = () => {
           <div
             className={cn(
               "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-              pathname.split("/")[1] !== "dashboard" && "group-hover:h-[20px]",
-              pathname.split("/")[1] === "dashboard" ? "h-[36px]" : "h-[8px]"
+              pathname?.split("/")[1] !== "dashboard" && "group-hover:h-[20px]",
+              pathname?.split("/")[1] === "dashboard" ? "h-[36px]" : "h-[8px]"
             )}
           />
           <div
             className={cn(
               "flex mx-3 h-[48px] w-[48px] rounded-[24px]  transition-all overflow-hidden items-center justify-center bg-neutral-700",
-              pathname.split("/")[1] !== "dashboard" && "group-hover:rounded-[16px]  group-hover:bg-blue-500",
-              pathname.split("/")[1] === "dashboard" && "bg-blue-500 rounded-[16px]"
+              pathname?.split("/")[1] !== "dashboard" && "group-hover:rounded-[16px]  group-hover:bg-blue-500",
+              pathname?.split("/")[1] === "dashboard" && "bg-blue-500 rounded-[16px]"
             )}
           >
             <Icons.logo className="w-10 h-10" />
